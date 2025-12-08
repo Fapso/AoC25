2d2a094d0e3ec3a3d10d39ba661d1ba8aba6815c
Fapso <adamjaworowski@o2.pl>
2025-12-08 17:30:09 +0100

    Eight

---
+++ Eight/eight_task_part_one.py
@@ -0,0 +1,95 @@
+import math
+
+# file = open("eight_task_test.txt")
+file = open("eight_task_puzzle_input.txt")
+
+
+def calculate_distance(x1, y1, z1, x2, y2, z2):
+    d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+    return d
+
+
+positions = []
+
+for line in file:
+    line = line.rstrip("\n")
+    line = line.split(",")
+    positions.append(line)
+
+
+def merry_go_around(times):
+    print(f"**** {times}")
+    distances_shortest = []
+
+    already_checked = set()
+
+    for i, p in enumerate(positions):
+        x1, y1, z1 = map(int, p)
+
+        distances = []
+
+        for x, q in enumerate(positions):
+            if i == x:
+                continue
+            if x in already_checked:
+                continue
+
+            x2, y2, z2 = map(int, q)
+            distance = calculate_distance(x1, y1, z1, x2, y2, z2)
+
+            distances.append([i, x, distance])
+
+        already_checked.add(i)
+
+        if not distances:
+            continue
+
+        for dist in distances:
+            distances_shortest.append(dist)
+
+    distances_shortest.sort(key=lambda t: t[2])
+    picked_connections = []
+    for t in range(times):
+        picked_connections.append(distances_shortest[t])
+
+    all_positions = set()
+    for x, y, _ in distances_shortest:
+        all_positions.add(x)
+        all_positions.add(y)
+
+    circuits = []
+    for p in all_positions:
+        circuits.append(set([p]))
+
+    for pair in picked_connections[:times]:
+        x1, y1, _ = pair
+        if any([x1 in s and y1 in s for s in circuits]):
+            continue
+
+        set_with_x1 = None
+        for i, s in enumerate(circuits):
+            if x1 in s:
+                set_with_x1 = s
+                del circuits[i]
+
+        set_with_y1 = None
+        for i, s in enumerate(circuits):
+            if y1 in s:
+                set_with_y1 = s
+                del circuits[i]
+
+        combined_set = set_with_x1 | set_with_y1
+        circuits.append(combined_set)
+
+    circuits_list = [len(x) for x in circuits]
+    circuits_list.sort(key=lambda t: t, reverse=True)
+    cut = circuits_list[0:3]
+    a = 1
+    for cu in cut:
+        a *= cu
+
+    print(a)
+
+
+# merry_go_around(10)
+merry_go_around(1000)
